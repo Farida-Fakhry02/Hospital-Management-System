ee59e211d28daa68f0264a4cf6e4fb08dba14ad7
Monica Hany <126457536+Monica-hany@users.noreply.github.com>
2024-04-22 21:24:35 +0200

    receptionist class

---
+++ MainModule/src/Receptionist.java
@@ -0,0 +1,31 @@
+
+public class Receptionist {
+
+    private String username;
+    private String password;
+
+    public Receptionist(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean verifyCredentials(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
